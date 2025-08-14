217d575fc7ea7030c932b071e1412b2f7fa389dd
Borys <boris02374@gmail.com>
2025-08-14 22:45:20 +0300

    fixed hw-2

--- conf/db.py
+++ conf/db.py
@@ -1,12 +1,16 @@
-import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-SQLALCHEMY_DATABASE_URL = os.environ.get("SQLALCHEMY_DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://postgres:567234@postgres_db:5432/hw02"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True, max_overflow=5)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base() # The Base class is created
+
+
+class Base(DeclarativeBase):
+    pass
+
 
 # Dependency
 def get_db():
